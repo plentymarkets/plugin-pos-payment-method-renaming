47e4dcab7ee31699534a0ef0fe1a9b576de0608c
Marcel Pönninghaus <marcel.poenninghaus@plentymarkets.com>
2020-04-21 09:42:24 +0200

    key update

--- src/Assistants/Handlers/PaymentMethodRenamingSettingsHandler.php
+++ src/Assistants/Handlers/PaymentMethodRenamingSettingsHandler.php
@@ -37,19 +37,19 @@ class PaymentMethodRenamingSettingsHandler implements WizardSettingsHandler
 
                 /** @var Configuration $deNameConfig */
                 $deCashNameConfig = $paymentMethodRenamingPlugin->configurations->where('key','cash.nameDE')->first();
-                $configuration[] = ['key' => $deCashNameConfig->key, 'value' => $data['cashNameDE']];
+                $configuration[] = ['key' => 'cash.nameDE', 'value' => $data['cashNameDE']];
 
                 /** @var Configuration $enNameConfig */
                 $enCashNameConfig = $paymentMethodRenamingPlugin->configurations->where('key','cash.nameEN')->first();
-                $configuration[] = ['key' => $enCashNameConfig->key, 'value' => $data['cashNameEN']];
+                $configuration[] = ['key' => 'cash.nameEN', 'value' => $data['cashNameEN']];
 
                 /** @var Configuration $deNameConfig */
                 $dePaymentCardNameConfig = $paymentMethodRenamingPlugin->configurations->where('key','paymentCard.nameDE')->first();
-                $configuration[] = ['key' => $dePaymentCardNameConfig->key, 'value' => $data['paymentCardNameDE']];
+                $configuration[] = ['key' => 'paymentCard.nameDE', 'value' => $data['paymentCardNameDE']];
 
                 /** @var Configuration $enNameConfig */
                 $enPaymentCardNameConfig = $paymentMethodRenamingPlugin->configurations->where('key','paymentCard.nameEN')->first();
-                $configuration[] = ['key' => $enPaymentCardNameConfig->key, 'value' => $data['paymentCardNameEN']];
+                $configuration[] = ['key' => 'paymentCard.nameEN', 'value' => $data['paymentCardNameEN']];
 
                 $configRepo->saveConfiguration($paymentMethodRenamingPlugin->id, $configuration);
             }
